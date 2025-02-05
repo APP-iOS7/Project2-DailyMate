a6262e54518cfaec82f68c31f2148211d675e559
Hyuk <hyukho83@gmail.com>
2025-02-05 13:20:32 +0900

    앱 재시작시 데이터지워지는 문제 해결

--- DailyMate/DailyMateApp.swift
+++ DailyMate/DailyMateApp.swift
@@ -10,23 +10,18 @@ import SwiftData
 
 @main
 struct DailyMateApp: App {
-    var sharedModelContainer: ModelContainer = {
+    let sharedModelContainer: ModelContainer = {
         let schema = Schema([
             DayItem.self,
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        // 앱의 문서 디렉토리에서 SwiftData 저장소 URL 얻기
-        let url = URL.documentsDirectory.appendingPathComponent("DailyMate.store")
         
-        // 기존 저장소 삭제
-        try? FileManager.default.removeItem(at: url)
-
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
-            // 마지막 시도: 메모리에만 저장하는 설정으로 시도
-            let inMemoryConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+            // 오류 발생 시 새로운 설정으로 다시 시도
+            let inMemoryConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
             do {
                 return try ModelContainer(for: schema, configurations: [inMemoryConfiguration])
             } catch {
